4fcceb9d48ef18e4cfebb80e7f032d700f8e72b9
craigatk <atk.craig@gmail.com>
2020-01-01 14:02:26 -0600

    Updating jest test match expression so it works on Windows

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "<rootDir>/src"
   ],
   preset: 'ts-jest',
-  testMatch: ['<rootDir>/src/**/?(*.)spec.{ts,tsx}'],
+  testMatch: ['**/*.spec.{ts,tsx}'],
 }
 
 process.env = Object.assign(process.env, { API_BASE_URL: 'http://localhost:8080/' });
