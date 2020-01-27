6916c5d704d628d4bfbd34b72544e451a0ce866e
craigatk <atk.craig@gmail.com>
2020-01-27 08:03:49 -0600

    Updating to use a Node script in publisher

---
+++ publishers/node-script/DEVELOPMENT.md
@@ -0,0 +1,8 @@
+# Node publisher development
+
+## Local install
+
+To install this package locally to try it out before publishing,
+for example from the `ui` directory:
+
+` yarn add -D file:../publishers/node-script`
--- publishers/node-script/src/index.js
+++ publishers/node-script/src/index.js
@@ -1,17 +1,23 @@
-const argv = require("minimist")(process.argv.slice(2));
-const { collectResults, sendResults } = require("./publish");
+function run(args) {
+  const argv = require("minimist")(args);
+  const { collectResults, sendResults } = require("./publish");
 
-const serverUrl = argv.serverUrl;
-const resultsFileGlobs = argv._;
+  const serverUrl = argv.serverUrl;
+  const resultsFileGlobs = argv._;
 
-console.log(
-  `Gathering results from ${resultsFileGlobs} to send to Projektor server ${serverUrl}`
-);
+  console.log(
+    `Gathering results from ${resultsFileGlobs} to send to Projektor server ${serverUrl}`
+  );
 
-const resultsBlob = collectResults(resultsFileGlobs);
+  const resultsBlob = collectResults(resultsFileGlobs);
 
-if (resultsBlob.length > 0) {
-  sendResults(resultsBlob, serverUrl).then(respData => {
-    console.log(`View Projektor results at ${serverUrl}${respData.uri}`);
-  });
+  if (resultsBlob.length > 0) {
+    sendResults(resultsBlob, serverUrl).then(respData => {
+      console.log(`View Projektor results at ${serverUrl}${respData.uri}`);
+    });
+  }
 }
+
+module.exports = {
+  run
+};
