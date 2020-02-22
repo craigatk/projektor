a58a2aa9fbc657f0b1b0e4cb4597ff6776c6b621
craigatk <atk.craig@gmail.com>
2020-02-22 07:59:45 -0600

    (test) Adding test for deleting object from store

--- server/object-store/src/test/kotlin/projektor/objectstore/ObjectStoreClientObjectSpec.kt
+++ server/object-store/src/test/kotlin/projektor/objectstore/ObjectStoreClientObjectSpec.kt
@@ -1,8 +1,11 @@
 package projektor.objectstore
 
 import io.kotlintest.specs.StringSpec
+import io.minio.errors.ErrorResponseException
 import strikt.api.expectThat
+import strikt.assertions.containsIgnoringCase
 import strikt.assertions.isEqualTo
+import strikt.assertions.isNotNull
 
 @ExperimentalStdlibApi
 class ObjectStoreClientObjectSpec : StringSpec() {
@@ -19,6 +22,20 @@ class ObjectStoreClientObjectSpec : StringSpec() {
             client.putObject(bucketName, objectName, "src/test/resources/test_file.txt")
 
             expectThat(client.getObject(bucketName, objectName).readBytes().decodeToString()).isEqualTo("Here is a test file")
+        }
+
+        "should delete object"() {
+            client.putObject(bucketName, objectName, "src/test/resources/test_file.txt")
+
+            client.removeObject(bucketName, objectName)
+
+            try {
+                client.getObject(bucketName, objectName)
+            } catch (e: ErrorResponseException) {
+                expectThat(e.message).isNotNull().and {
+                    containsIgnoringCase("does not exist")
+                }
+            }
         }
     }
 }
