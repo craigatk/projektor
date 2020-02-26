768374870560a0f1e116d230176abf79d2e123d5
craigatk <atk.craig@gmail.com>
2020-02-26 16:28:44 -0600

    (fix) Making attachment functional test more robust

--- functional-test/cypress/support/commands.js
+++ functional-test/cypress/support/commands.js
@@ -1,43 +1,47 @@
 Cypress.Commands.add("loadFixture", (fixturePath, visitUri = "") =>
-    cy.readFile(
-        fixturePath
-    ).then(resultsBlob =>
-        cy.request("POST", `http://localhost:8080/results`, resultsBlob).then(resp =>
-            cy.visit(`http://localhost:8080/tests/${resp.body.id}${visitUri}`, {
-                retryOnStatusCodeFailure: true
-            })
-        )
-    )
+  cy.readFile(fixturePath).then(resultsBlob =>
+    cy
+      .request("POST", `http://localhost:8080/results`, resultsBlob)
+      .then(resp =>
+        cy.visit(`http://localhost:8080/tests/${resp.body.id}${visitUri}`, {
+          retryOnStatusCodeFailure: true
+        })
+      )
+  )
 );
 
 Cypress.Commands.add("loadGroupedFixture", (fixturePath, visitUri = "") =>
-    cy.readFile(
-        fixturePath
-    ).then(resultsBlob =>
-        cy.request("POST", `http://localhost:8080/groupedResults`, resultsBlob).then(resp =>
-            cy.visit(`http://localhost:8080/tests/${resp.body.id}${visitUri}`, {
-                retryOnStatusCodeFailure: true
-            })
-        )
-    )
+  cy.readFile(fixturePath).then(resultsBlob =>
+    cy
+      .request("POST", `http://localhost:8080/groupedResults`, resultsBlob)
+      .then(resp =>
+        cy.visit(`http://localhost:8080/tests/${resp.body.id}${visitUri}`, {
+          retryOnStatusCodeFailure: true
+        })
+      )
+  )
 );
 
-Cypress.Commands.add("loadGroupedFixtureWithAttachment", (fixturePath, attachmentPath, attachmentName, visitUri = "") =>
-    cy.readFile(
-        attachmentPath
-    ).then(attachmentContents => {
-        cy.readFile(
-            fixturePath
-        ).then(resultsBlob =>
-            cy.request("POST", `http://localhost:8080/groupedResults`, resultsBlob).then(resp => {
-                const publicId = resp.body.id;
-                cy.request("POST", `http://localhost:8080/run/${publicId}/attachments/${attachmentName}`, attachmentContents).then(() => {
-                    cy.visit(`http://localhost:8080/tests/${publicId}${visitUri}`, {
-                        retryOnStatusCodeFailure: true
-                    })
-                })
-
-            })
-        )
+Cypress.Commands.add(
+  "loadGroupedFixtureWithAttachment",
+  (fixturePath, attachmentPath, attachmentName, visitUri = "") =>
+    cy.readFile(attachmentPath).then(attachmentContents => {
+      cy.readFile(fixturePath).then(resultsBlob =>
+        cy
+          .request("POST", `http://localhost:8080/groupedResults`, resultsBlob)
+          .then(resp => {
+            const publicId = resp.body.id;
+            cy.request({
+              method: "POST",
+              url: `http://localhost:8080/run/${publicId}/attachments/${attachmentName}`,
+              body: attachmentContents,
+              retryOnStatusCodeFailure: true
+            }).then(() => {
+              cy.visit(`http://localhost:8080/tests/${publicId}${visitUri}`, {
+                retryOnStatusCodeFailure: true
+              });
+            });
+          })
+      );
     })
 );
