0803389baf5326355e461d12cd9732f0e68d7d5b
craigatk <atk.craig@gmail.com>
2020-03-14 13:05:21 -0500

    (test) Attempting testing on Java 8 in CI

--- publishers/gradle-plugin/src/test/groovy/projektor/plugin/testkit/version/GradleVersionSingleProjectSpec.groovy
+++ publishers/gradle-plugin/src/test/groovy/projektor/plugin/testkit/version/GradleVersionSingleProjectSpec.groovy
@@ -4,9 +4,8 @@ import com.github.tomakehurst.wiremock.verification.LoggedRequest
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.util.GradleVersion
 import projektor.plugin.testkit.SingleProjectSpec
+import spock.lang.Requires
 import spock.lang.Unroll
-
-import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
 class GradleVersionSingleProjectSpec extends SingleProjectSpec {
     @Unroll
@@ -45,5 +44,37 @@ class GradleVersionSingleProjectSpec extends SingleProjectSpec {
         "5.0"                           | _
         "6.0.1"                         | _
         GradleVersion.current().version | _
+    }
+
+    @Requires({ jvm.isJava8() })
+    def "should publish results from test task to server with Gradle version 4.0 (requires Java 8)"() {
+        given:
+        buildFile << """
+            projektor {
+                serverUrl = '${serverUrl}'
+            }
+        """.stripIndent()
+
+        File testDirectory = specWriter.createTestDirectory(projectRootDir)
+        specWriter.writeFailingSpecFile(testDirectory, "SampleSpec")
+
+        String resultsId = "ABC123"
+        resultsStubber.stubResultsPostSuccess(resultsId)
+
+        when:
+        def result = GradleRunner.create()
+                .withProjectDir(projectRootDir.root)
+                .withArguments('test')
+                .withPluginClasspath()
+                .withGradleVersion("4.0")
+                .buildAndFail()
+
+        then:
+        !result.output.contains("Projektor plugin enabled but no server specified")
+        result.output.contains("View Projektor report at: ${serverUrl}/tests/${resultsId}")
+
+        and:
+        List<LoggedRequest> resultsRequests = resultsStubber.findResultsRequests()
+        resultsRequests.size() == 1
     }
 }
