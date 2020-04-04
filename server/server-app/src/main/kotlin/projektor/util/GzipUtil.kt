93516ece9f29abc4506bb25aca5d321ead7ef56f
craigatk <atk.craig@gmail.com>
2020-04-04 10:50:14 -0500

    (feature) Accepting gzipped results

--- server/server-app/src/main/kotlin/projektor/util/GzipUtil.kt
+++ server/server-app/src/main/kotlin/projektor/util/GzipUtil.kt
@@ -1,4 +1,14 @@
+package projektor.util
+
+import java.io.ByteArrayOutputStream
 import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
+
+fun gzip(content: String): ByteArray {
+    val bos = ByteArrayOutputStream()
+    GZIPOutputStream(bos).bufferedWriter(Charsets.UTF_8).use { it.write(content) }
+    return bos.toByteArray()
+}
 
 fun ungzip(content: ByteArray): String =
         GZIPInputStream(content.inputStream()).bufferedReader(Charsets.UTF_8).use { it.readText() }
