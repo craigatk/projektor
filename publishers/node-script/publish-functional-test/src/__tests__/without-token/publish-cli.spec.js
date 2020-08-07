c35fe9115092fc372d2e9b00cca9b5addbdba6fd
craigatk <atk.craig@gmail.com>
2020-08-06 19:47:30 -0500

    (chore) Making Node script functional test more resilient

--- publishers/node-script/publish-functional-test/src/__tests__/without-token/publish-cli.spec.js
+++ publishers/node-script/publish-functional-test/src/__tests__/without-token/publish-cli.spec.js
@@ -1,4 +1,5 @@
 const { exec, execSync } = require("child_process");
+const waitForExpect = require("wait-for-expect");
 const { extractTestRunId } = require("../util/parse_output");
 const { fetchTestRunSummary } = require("../util/projektor_client");
 const { verifyOutput } = require("../verify/cli_output_verify");
@@ -18,14 +19,16 @@ describe("Publishing via CLI", () => {
         const testRunId = extractTestRunId(stdout);
         console.log("Test ID", testRunId);
 
-        const testRunSummaryResponse = await fetchTestRunSummary(
-          testRunId,
-          serverPort
-        );
-        expect(testRunSummaryResponse.status).toEqual(200);
+        await waitForExpect(async () => {
+          const testRunSummaryResponse = await fetchTestRunSummary(
+            testRunId,
+            serverPort
+          );
+          expect(testRunSummaryResponse.status).toEqual(200);
 
-        expect(testRunSummaryResponse.data.id).toEqual(testRunId);
-        expect(testRunSummaryResponse.data.total_test_count).toEqual(4);
+          expect(testRunSummaryResponse.data.id).toEqual(testRunId);
+          expect(testRunSummaryResponse.data.total_test_count).toEqual(4);
+        });
 
         done();
       }
