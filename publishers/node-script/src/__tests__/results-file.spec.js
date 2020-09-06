c896c0dffa1c406d6860d06f04e4b7e9c479ad3e
craigatk <atk.craig@gmail.com>
2020-09-06 08:37:21 -0500

    (fix) Fixing snake_case vs camelCase in Node script results file

--- publishers/node-script/src/__tests__/results-file.spec.js
+++ publishers/node-script/src/__tests__/results-file.spec.js
@@ -1,9 +1,13 @@
-const { writeResultsFileToDisk } = require("../results-file");
+const {
+  writeResultsFileToDisk,
+  readResultsFileFromDisk,
+  defaultResultsFileName,
+} = require("../results-file");
 
 const fs = require("fs");
 
 describe("results file", () => {
-  const resultsFileName = "projektor_report.json";
+  const resultsFileName = defaultResultsFileName;
 
   afterEach(() => {
     if (fs.existsSync(resultsFileName)) {
@@ -20,9 +24,23 @@ describe("results file", () => {
     const resultsFileContents = fs.readFileSync(resultsFileName).toString();
     console.log("Results file contents", resultsFileContents);
 
+    expect(resultsFileContents).toContain("report_url");
+
     const resultsFileJson = JSON.parse(resultsFileContents);
 
     expect(resultsFileJson.id).toBe(publicId);
-    expect(resultsFileJson.reportUrl).toBe(reportUrl);
+    expect(resultsFileJson.report_url).toBe(reportUrl);
+  });
+
+  it("should read results from file", () => {
+    const publicId = "RES123";
+    const reportUrl = "http://localhost:8080/tests/RES123";
+
+    writeResultsFileToDisk(publicId, reportUrl, resultsFileName);
+
+    const results = readResultsFileFromDisk(resultsFileName);
+
+    expect(results.id).toBe(publicId);
+    expect(results.reportUrl).toBe(reportUrl);
   });
 });
--- publishers/node-script/src/results-file.js
+++ publishers/node-script/src/results-file.js
@@ -1,14 +1,25 @@
 const fs = require("fs");
 
+const defaultResultsFileName = "projektor_report.json";
+
 function writeResultsFileToDisk(publicId, reportUrl, resultsFileName) {
   const resultsFileJson = {
     id: publicId,
-    reportUrl,
+    report_url: reportUrl,
   };
 
   fs.writeFileSync(resultsFileName, JSON.stringify(resultsFileJson));
 }
 
+function readResultsFileFromDisk(resultsFileName) {
+  const resultsFileContents = fs.readFileSync(resultsFileName).toString();
+  const results = JSON.parse(resultsFileContents);
+
+  return { id: results.id, reportUrl: results.report_url };
+}
+
 module.exports = {
   writeResultsFileToDisk,
+  readResultsFileFromDisk,
+  defaultResultsFileName,
 };
