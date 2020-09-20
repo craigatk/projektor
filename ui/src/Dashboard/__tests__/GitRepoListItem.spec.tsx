208320ab26acf3e858f3d38cc01ef698820ea621
craigatk <atk.craig@gmail.com>
2020-09-20 09:07:15 -0500

    (feature) Adding repo link on dashboard summary

---
+++ ui/src/Dashboard/__tests__/GitRepoListItem.spec.tsx
@@ -0,0 +1,41 @@
+import "@testing-library/jest-dom/extend-expect";
+import React from "react";
+import { render } from "@testing-library/react";
+import { TestRunGitMetadata } from "../../model/TestRunModel";
+import GitRepoListItem from "../GitRepoListItem";
+
+describe("GitRepoListItem", () => {
+  it("should display org link", () => {
+    const gitMetadata = {
+      orgName: "my-org",
+      repoName: "my-org/my-cov",
+      branchName: "main",
+      isMainBranch: true,
+    } as TestRunGitMetadata;
+
+    const { getByTestId } = render(
+      <GitRepoListItem gitMetadata={gitMetadata} />
+    );
+
+    expect(getByTestId("dashboard-summary-git-org-link")).toHaveTextContent(
+      "my-org"
+    );
+  });
+
+  it("should display repo link", () => {
+    const gitMetadata = {
+      orgName: "my-org",
+      repoName: "my-org/my-cov",
+      branchName: "main",
+      isMainBranch: true,
+    } as TestRunGitMetadata;
+
+    const { getByTestId } = render(
+      <GitRepoListItem gitMetadata={gitMetadata} />
+    );
+
+    expect(getByTestId("dashboard-summary-git-repo-link")).toHaveTextContent(
+      "my-cov"
+    );
+  });
+});
