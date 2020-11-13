078b22cc838c84f399bb1b746583fbc3fe96f46d
Craig Atkinson <atk.craig@gmail.com>
2020-11-12 19:33:13 -0600

    (feature) Ability to specify repository name on Node script (#228)

--- publishers/node-script/publish-functional-test/src/__tests__/without-token/publish-performance.spec.js
+++ publishers/node-script/publish-functional-test/src/__tests__/without-token/publish-performance.spec.js
@@ -1,7 +1,11 @@
 const { exec } = require("child_process");
 const waitForExpect = require("wait-for-expect");
 const { extractTestRunId } = require("../util/parse_output");
-const { fetchPerformanceResults } = require("../util/projektor_client");
+const {
+  fetchPerformanceResults,
+  fetchGitMetadata,
+  fetchTestRunSummary,
+} = require("../util/projektor_client");
 const { verifyOutput } = require("../verify/cli_output_verify");
 
 describe("Publishing performance results via CLI", () => {
@@ -27,6 +31,44 @@ describe("Publishing performance results via CLI", () => {
           );
           expect(performanceResultsResponse.status).toEqual(200);
         });
+
+        done();
+      }
+    );
+  });
+
+  it("should publish performance results with repository name", async (done) => {
+    exec(
+      `yarn projektor-publish --serverUrl=http://localhost:${serverPort} --performance=performance/getRun.json --repositoryName=projektor/projektor-app`,
+      async (error, stdout, stderr) => {
+        verifyOutput(error, stdout, stderr, serverPort);
+        expect(error).toBeNull();
+        expect(stdout).not.toContain(
+          "No test results files found in locations"
+        );
+
+        const testRunId = extractTestRunId(stdout);
+        console.log("Test ID", testRunId);
+
+        await waitForExpect(async () => {
+          const testRunSummaryResponse = await fetchTestRunSummary(
+            testRunId,
+            serverPort
+          );
+          expect(testRunSummaryResponse.status).toEqual(200);
+        });
+
+        const gitMetadataResponse = await fetchGitMetadata(
+          testRunId,
+          serverPort
+        );
+        expect(gitMetadataResponse.status).toEqual(200);
+
+        console.log("Git metadata response", gitMetadataResponse.data);
+
+        expect(gitMetadataResponse.data.repo_name).toEqual(
+          "projektor/projektor-app"
+        );
 
         done();
       }
