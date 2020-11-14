91345f0b57f572b182148b17f669e034e66ba074
craigatk <atk.craig@gmail.com>
2020-11-14 15:17:28 -0600

    (feature) Logging error response from server in Node script

---
+++ publishers/node-script/publish-functional-test/src/__tests__/without-token/publish-invalid-results.spec.js
@@ -0,0 +1,22 @@
+const { exec } = require("child_process");
+
+describe("publishing invalid test results", () => {
+  const serverPort = "8082";
+  const serverUrl = `http://localhost:${serverPort}`;
+
+  it("should log error message from server when publishing invalid test results", async (done) => {
+    exec(
+      `yarn projektor-publish --serverUrl=${serverUrl} results-invalid/*.xml`,
+      async (error, stdout, stderr) => {
+        expect(stderr).toContain(
+          `Error publishing results to Projektor server ${serverUrl}`
+        );
+        expect(stderr).toContain(`Request failed with status code 400`);
+        expect(stderr).toContain(`Error from server`);
+        expect(stderr).toContain(`Unexpected close tag`);
+
+        done();
+      }
+    );
+  });
+});
