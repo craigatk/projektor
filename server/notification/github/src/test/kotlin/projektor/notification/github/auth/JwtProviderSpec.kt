18a481b63fad6aa3380e3afe203e67a0f0ff0f6c
craigatk <atk.craig@gmail.com>
2020-12-19 14:59:24 -0600

    (feature) Including project name in GitHub comment

---
+++ server/notification/github/src/test/kotlin/projektor/notification/github/auth/JwtProviderSpec.kt
@@ -0,0 +1,30 @@
+package projektor.notification.github.auth
+
+import com.auth0.jwt.JWT
+import io.kotest.core.spec.style.StringSpec
+import strikt.api.expectThat
+import strikt.assertions.*
+
+class JwtProviderSpec : StringSpec() {
+    init {
+        "should create JWT from private key" {
+            val jwtTokenConfig = JwtTokenConfig(
+                gitHubAppId = "12345",
+                pemContents = loadTextFromFile("fake_private_key.txt"),
+                ttlMillis = 60_000
+            )
+
+            val jwtProvider = JwtProvider(jwtTokenConfig)
+
+            val token = jwtProvider.createJWT()
+
+            val decodedJwt = JWT().decodeJwt(token)
+            expectThat(decodedJwt).isNotNull()
+        }
+    }
+
+    private fun loadTextFromFile(filename: String) = javaClass
+        .getResourceAsStream("/$filename")
+        .bufferedReader()
+        .readText()
+}
