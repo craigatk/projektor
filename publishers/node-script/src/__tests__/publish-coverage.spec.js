ddfb3979ccdcddf54fa12d58df22054580c2ce4d
Craig Atkinson <atk.craig@gmail.com>
2020-12-22 06:55:52 -0600

    (feature) Sending coverage along with results and including commit SHA (#260)

--- publishers/node-script/src/__tests__/publish-coverage.spec.js
+++ publishers/node-script/src/__tests__/publish-coverage.spec.js
@@ -22,17 +22,13 @@ describe("publish with coverage", () => {
       .onPost("http://localhost:8080/groupedResults")
       .reply(200, { id: "ABC123", uri: "/tests/ABC123" });
 
-    mockAxios
-      .onPost("http://localhost:8080/run/ABC123/coverageFile")
-      .reply(200);
-
     const publishToken = "myPublishToken";
 
     await collectAndSendResults(serverUrl, publishToken, [fileGlob], null, [
       coverageGlob,
     ]);
 
-    expect(mockAxios.history.post.length).toBe(2);
+    expect(mockAxios.history.post.length).toBe(1);
 
     const resultsPostRequest = mockAxios.history.post.find((postRequest) =>
       postRequest.url.includes("groupedResults")
@@ -43,10 +39,11 @@ describe("publish with coverage", () => {
     expect(resultsPostData).toContain("resultsDir1-results1");
     expect(resultsPostData).toContain("resultsDir1-results2");
 
-    const coveragePostRequest = mockAxios.history.post.find((postRequest) =>
-      postRequest.url.includes("/run/ABC123/coverageFile")
+    const resultsPostBody = JSON.parse(resultsPostData);
+    expect(resultsPostBody.coverageFiles.length).toBe(1);
+    expect(resultsPostBody.coverageFiles[0].reportContents).toContain(
+      "<coverage"
     );
-    expect(coveragePostRequest.headers["X-PROJEKTOR-TOKEN"]).toBe(publishToken);
   });
 
   it("should include base directory path when publishing coverage", async () => {
@@ -75,39 +72,20 @@ describe("publish with coverage", () => {
       null,
       null,
       null,
+      null,
       false,
       "base/dir"
     );
 
-    const coveragePostRequest = mockAxios.history.post.find((postRequest) =>
-      postRequest.url.includes("/run/ABC123/coverageFile")
+    const resultsPostRequest = mockAxios.history.post.find((postRequest) =>
+      postRequest.url.includes("groupedResults")
     );
-    const coveragePostData = coveragePostRequest.data;
-    expect(coveragePostData).toContain("base/dir");
-  });
+    const resultsPostData = resultsPostRequest.data;
+    const resultsPostBody = JSON.parse(resultsPostData);
 
-  it("when publishing coverage fails should not fail build", async () => {
-    const fileGlob = "src/__tests__/resultsDir1/*.xml";
-    const coverageGlob = "src/__tests__/coverageDir1/*.xml";
-    const serverUrl = "http://localhost:8080";
-
-    mockAxios
-      .onPost("http://localhost:8080/groupedResults")
-      .reply(200, { id: "FAIL123", uri: "/tests/FAIL123" });
-
-    mockAxios
-      .onPost("http://localhost:8080/run/FAIL123/coverageFile")
-      .reply(400, null);
-
-    await collectAndSendResults(serverUrl, null, [fileGlob], null, [
-      coverageGlob,
-    ]);
-
-    expect(mockAxios.history.post.length).toBe(2);
-
-    const coveragePostRequest = mockAxios.history.post.find((postRequest) =>
-      postRequest.url.includes("run/FAIL123/coverageFile")
+    expect(resultsPostBody.coverageFiles.length).toBe(1);
+    expect(resultsPostBody.coverageFiles[0].baseDirectoryPath).toEqual(
+      "base/dir"
     );
-    expect(coveragePostRequest).not.toBeNull();
   });
 });
