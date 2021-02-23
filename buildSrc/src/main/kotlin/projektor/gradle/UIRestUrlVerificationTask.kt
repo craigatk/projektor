49b4eb6454cbf7849770e502bd55b758b3b4162c
Craig Atkinson <atk.craig@gmail.com>
2021-02-23 06:48:00 -0600

    (fix) Include verification in the build that UI production bundle isn't using localhost as the base URL (#304)

---
+++ buildSrc/src/main/kotlin/projektor/gradle/UIRestUrlVerificationTask.kt
@@ -0,0 +1,35 @@
+package projektor.gradle
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.VerificationTask
+import java.io.File
+
+open class UIRestUrlVerificationTask : DefaultTask(), VerificationTask {
+    private var ignoreFailures = false
+
+    override fun setIgnoreFailures(ignoreFailures: Boolean) {
+        this.ignoreFailures = ignoreFailures
+    }
+
+    override fun getIgnoreFailures(): Boolean =
+        this.ignoreFailures
+
+    @TaskAction
+    fun run() {
+        val jsDistDir = File(project.projectDir, "dist/")
+
+        val jsBundleFile = jsDistDir.walk().filter { it.extension == "js" }.firstOrNull()
+
+        if (jsBundleFile != null) {
+            val jsBundleFileContents = jsBundleFile.readText()
+
+            if (jsBundleFileContents.contains("http://localhost:8080/")) {
+                throw GradleException("JS production bundle contains localhost URL. File ${jsBundleFile.absolutePath}")
+            }
+        } else {
+            throw GradleException("Could not find JS bundle file in directory ${jsDistDir.absolutePath}")
+        }
+    }
+}
