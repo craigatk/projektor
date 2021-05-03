7227b6229b5f3c99ae34b88d16f96082044936e2
Craig Atkinson <atk.craig@gmail.com>
2021-05-03 07:17:18 -0500

    (test) Adding additional tests for appending test results (#338)

--- server/server-app/src/test/kotlin/projektor/incomingresults/AppendGroupedResultsApplicationTest.kt
+++ server/server-app/src/test/kotlin/projektor/incomingresults/AppendGroupedResultsApplicationTest.kt
@@ -1,7 +1,7 @@
 package projektor.incomingresults
 
-import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpMethod
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.ktor.http.*
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.setBody
 import io.ktor.server.testing.withTestApplication
@@ -17,6 +17,7 @@ import projektor.parser.grouped.model.GitMetadata
 import projektor.parser.grouped.model.ResultsMetadata
 import projektor.server.api.PublicId
 import projektor.server.api.results.ResultsProcessingStatus
+import projektor.server.api.results.SaveResultsResponse
 import strikt.api.expectThat
 import strikt.assertions.hasSize
 import strikt.assertions.isEqualTo
@@ -28,7 +29,7 @@ import kotlin.test.assertNotNull
 class AppendGroupedResultsApplicationTest : ApplicationTestCase() {
 
     @Test
-    fun `should append grouped test results`() {
+    fun `should append second grouped test results run`() {
         val repoPart = RandomStringUtils.randomAlphabetic(12)
 
         val gitMetadata = GitMetadata()
@@ -87,6 +88,15 @@ class AppendGroupedResultsApplicationTest : ApplicationTestCase() {
                 addHeader(HttpHeaders.ContentType, "application/json")
                 setBody(secondRequestBody)
             }.apply {
+                expectThat(response.status()).isEqualTo(HttpStatusCode.OK)
+
+                val responseContent = response.content
+                assertNotNull(responseContent)
+                val resultsResponse: SaveResultsResponse = objectMapper.readValue(responseContent)
+
+                val secondPublicIdResponse = resultsResponse.id
+                expectThat(secondPublicIdResponse).isEqualTo(publicId.id)
+
                 val (secondPublicId, _) = waitForTestRunSaveToComplete(response)
                 expectThat(secondPublicId).isEqualTo(publicId)
 
@@ -116,6 +126,159 @@ class AppendGroupedResultsApplicationTest : ApplicationTestCase() {
                 val testSuiteGroup1 = testSuiteGroups.find { it.groupName == "Group1" }
                 assertNotNull(testSuiteGroup1)
                 expectThat(testSuiteDao.fetchByTestSuiteGroupId(testSuiteGroup1.id)).hasSize(3)
+
+                val testSuiteGroup2 = testSuiteGroups.find { it.groupName == "Group2" }
+                assertNotNull(testSuiteGroup2)
+                expectThat(testSuiteDao.fetchByTestSuiteGroupId(testSuiteGroup2.id)).hasSize(1)
+            }
+        }
+    }
+
+    @Test
+    fun `should append second and third grouped test results runs`() {
+        val repoPart = RandomStringUtils.randomAlphabetic(12)
+
+        val gitMetadata = GitMetadata()
+        gitMetadata.repoName = "craigatk/$repoPart"
+        gitMetadata.branchName = "main"
+        gitMetadata.isMainBranch = true
+        val metadata = ResultsMetadata()
+        metadata.git = gitMetadata
+        metadata.group = "B12"
+
+        val initialRequestBody = GroupedResultsXmlLoader().passingGroupedResults(metadata)
+
+        lateinit var publicId: PublicId
+        lateinit var testRun: TestRun
+
+        withTestApplication(::createTestApplication) {
+            handleRequest(HttpMethod.Post, "/groupedResults") {
+                addHeader(HttpHeaders.ContentType, "application/json")
+                setBody(initialRequestBody)
+            }.apply {
+                val initialSaveComplete = waitForTestRunSaveToComplete(response)
+                publicId = initialSaveComplete.first
+                testRun = initialSaveComplete.second
+
+                expectThat(testRun) {
+                    get { passed }.isTrue()
+                    get { totalPassingCount }.isEqualTo(3)
+                    get { totalFailureCount }.isEqualTo(0)
+                    get { totalSkippedCount }.isEqualTo(0)
+                    get { totalTestCount }.isEqualTo(3)
+                }
+
+                val testSuites = testSuiteDao.fetchByTestRunId(testRun.id)
+                expectThat(testSuites).hasSize(3)
+
+                val testSuiteGroups = testSuiteGroupDao.fetchByTestRunId(testRun.id)
+                expectThat(testSuiteGroups).hasSize(2)
+
+                val testSuiteGroup1 = testSuiteGroups.find { it.groupName == "Group1" }
+                assertNotNull(testSuiteGroup1)
+                expectThat(testSuiteDao.fetchByTestSuiteGroupId(testSuiteGroup1.id)).hasSize(2)
+
+                val testSuiteGroup2 = testSuiteGroups.find { it.groupName == "Group2" }
+                assertNotNull(testSuiteGroup2)
+                expectThat(testSuiteDao.fetchByTestSuiteGroupId(testSuiteGroup2.id)).hasSize(1)
+
+                await until { resultsProcessingDao.fetchOneByPublicId(publicId.id).status == ResultsProcessingStatus.SUCCESS.name }
+            }
+        }
+
+        val secondRequestBody = GroupedResultsXmlLoader().wrapResultsXmlInGroup(resultsXmlLoader.failing(), metadata)
+
+        withTestApplication(::createTestApplication) {
+            handleRequest(HttpMethod.Post, "/groupedResults") {
+                addHeader(HttpHeaders.ContentType, "application/json")
+                setBody(secondRequestBody)
+            }.apply {
+                expectThat(response.status()).isEqualTo(HttpStatusCode.OK)
+
+                val responseContent = response.content
+                assertNotNull(responseContent)
+                val resultsResponse: SaveResultsResponse = objectMapper.readValue(responseContent)
+
+                val secondPublicIdResponse = resultsResponse.id
+                expectThat(secondPublicIdResponse).isEqualTo(publicId.id)
+
+                val (secondPublicId, _) = waitForTestRunSaveToComplete(response)
+                expectThat(secondPublicId).isEqualTo(publicId)
+
+                await until {
+                    val updatedTestRun = testRunDao.fetchOneByPublicId(publicId.id)
+                    !updatedTestRun.passed
+                }
+
+                val secondTestRun = testRunDao.fetchOneByPublicId(publicId.id)
+
+                expectThat(secondTestRun) {
+                    get { passed }.isFalse()
+                    get { totalPassingCount }.isEqualTo(3)
+                    get { totalFailureCount }.isEqualTo(2)
+                    get { totalSkippedCount }.isEqualTo(0)
+                    get { totalTestCount }.isEqualTo(5)
+                }
+
+                val testSuites = testSuiteDao.fetchByTestRunId(testRun.id)
+                expectThat(testSuites).hasSize(4)
+
+                val testSuiteGroups = testSuiteGroupDao.fetchByTestRunId(testRun.id)
+                expectThat(testSuiteGroups).hasSize(2)
+
+                val testSuiteGroup1 = testSuiteGroups.find { it.groupName == "Group1" }
+                assertNotNull(testSuiteGroup1)
+                expectThat(testSuiteDao.fetchByTestSuiteGroupId(testSuiteGroup1.id)).hasSize(3)
+
+                val testSuiteGroup2 = testSuiteGroups.find { it.groupName == "Group2" }
+                assertNotNull(testSuiteGroup2)
+                expectThat(testSuiteDao.fetchByTestSuiteGroupId(testSuiteGroup2.id)).hasSize(1)
+            }
+        }
+
+        val thirdRequestBody = GroupedResultsXmlLoader().wrapResultsXmlInGroup(resultsXmlLoader.someIgnored(), metadata)
+
+        withTestApplication(::createTestApplication) {
+            handleRequest(HttpMethod.Post, "/groupedResults") {
+                addHeader(HttpHeaders.ContentType, "application/json")
+                setBody(thirdRequestBody)
+            }.apply {
+                expectThat(response.status()).isEqualTo(HttpStatusCode.OK)
+
+                val responseContent = response.content
+                assertNotNull(responseContent)
+                val resultsResponse: SaveResultsResponse = objectMapper.readValue(responseContent)
+
+                val thirdPublicIdResponse = resultsResponse.id
+                expectThat(thirdPublicIdResponse).isEqualTo(publicId.id)
+
+                val (thirdPublicId, _) = waitForTestRunSaveToComplete(response)
+                expectThat(thirdPublicId).isEqualTo(publicId)
+
+                await until {
+                    val updatedTestRun = testRunDao.fetchOneByPublicId(publicId.id)
+                    updatedTestRun.totalSkippedCount > 0
+                }
+
+                val secondTestRun = testRunDao.fetchOneByPublicId(publicId.id)
+
+                expectThat(secondTestRun) {
+                    get { passed }.isFalse()
+                    get { totalPassingCount }.isEqualTo(10)
+                    get { totalFailureCount }.isEqualTo(2)
+                    get { totalSkippedCount }.isEqualTo(3)
+                    get { totalTestCount }.isEqualTo(15)
+                }
+
+                val testSuites = testSuiteDao.fetchByTestRunId(testRun.id)
+                expectThat(testSuites).hasSize(5)
+
+                val testSuiteGroups = testSuiteGroupDao.fetchByTestRunId(testRun.id)
+                expectThat(testSuiteGroups).hasSize(2)
+
+                val testSuiteGroup1 = testSuiteGroups.find { it.groupName == "Group1" }
+                assertNotNull(testSuiteGroup1)
+                expectThat(testSuiteDao.fetchByTestSuiteGroupId(testSuiteGroup1.id)).hasSize(4)
 
                 val testSuiteGroup2 = testSuiteGroups.find { it.groupName == "Group2" }
                 assertNotNull(testSuiteGroup2)
