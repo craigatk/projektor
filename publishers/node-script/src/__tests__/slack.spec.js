9ed4a49fd340bdf516fe6a21003e983113fc8d20
Craig Atkinson <atk.craig@gmail.com>
2021-06-09 05:17:27 -0500

    (fix) Write passing Slack message file in Node script

--- publishers/node-script/src/__tests__/slack.spec.js
+++ publishers/node-script/src/__tests__/slack.spec.js
@@ -11,14 +11,16 @@ describe("slack messages", () => {
     }
   });
 
-  it("should create slack message", () => {
+  it("should create slack message when tests failed", () => {
     const reportUrl = "http://localhost/tests/ABC12345";
     const projectName = "my-project";
+    const testsFailed = true;
     const currentTimestamp = 1596674144043;
 
     const messageJson = createSlackMessageJson(
       reportUrl,
       projectName,
+      testsFailed,
       currentTimestamp
     );
 
@@ -27,14 +29,52 @@ describe("slack messages", () => {
     );
   });
 
+  it("should create slack message when tests passed with project name", () => {
+    const reportUrl = "http://localhost/tests/ABC12345";
+    const projectName = "my-project";
+    const testsFailed = false;
+    const currentTimestamp = 1596674144043;
+
+    const messageJson = createSlackMessageJson(
+      reportUrl,
+      projectName,
+      testsFailed,
+      currentTimestamp
+    );
+
+    expect(messageJson).toEqual(
+      '{"attachments":[{"fallback":"Projektor test report","color":"#00FF00","pretext":"Tests passed in project my-project","title":"Projektor test report","title_link":"http://localhost/tests/ABC12345","text":"See the Projektor test report for details on the tests","footer":"Projektor","ts":1596674144043}]}'
+    );
+  });
+
+  it("should create slack message when tests passed without project name", () => {
+    const reportUrl = "http://localhost/tests/ABC12345";
+    const projectName = null;
+    const testsFailed = false;
+    const currentTimestamp = 1596674144043;
+
+    const messageJson = createSlackMessageJson(
+      reportUrl,
+      projectName,
+      testsFailed,
+      currentTimestamp
+    );
+
+    expect(messageJson).toEqual(
+      '{"attachments":[{"fallback":"Projektor test report","color":"#00FF00","pretext":"Tests passed in project ","title":"Projektor test report","title_link":"http://localhost/tests/ABC12345","text":"See the Projektor test report for details on the tests","footer":"Projektor","ts":1596674144043}]}'
+    );
+  });
+
   it("should write slack message file to disk", () => {
     const reportUrl = "http://localhost/tests/DEF12345";
     const projectName = "my-disk-project";
+    const testsFailed = true;
 
     writeSlackMessageFileToDisk(
       reportUrl,
       "projektor_failure_message.json",
-      projectName
+      projectName,
+      testsFailed
     );
 
     expect(fs.existsSync("projektor_failure_message.json")).toBeTruthy();
