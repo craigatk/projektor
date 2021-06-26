dbbbb0b1600f1a7a65aac06ca118f38c5274a210
Craig Atkinson <atk.craig@gmail.com>
2021-06-26 14:45:46 -0500

    (feature) Handle test run URL with trailing slash and upgrade Kotlin to 1.5.20

--- server/server-app/src/main/kotlin/projektor/route/TestRunRoutes.kt
+++ server/server-app/src/main/kotlin/projektor/route/TestRunRoutes.kt
@@ -18,8 +18,18 @@ fun Route.testRuns(testRunService: TestRunService) {
             testRunService.addTestRunSummaryToSpan(testRun.summary)
 
             call.respond(HttpStatusCode.OK, it)
-        }
-            ?: call.respond(HttpStatusCode.NotFound)
+        } ?: call.respond(HttpStatusCode.NotFound)
+    }
+    get("/run/{publicId}/") {
+        val publicId = call.parameters.getOrFail("publicId")
+
+        val testRun = testRunService.fetchTestRun(PublicId(publicId))
+
+        testRun?.let {
+            testRunService.addTestRunSummaryToSpan(testRun.summary)
+
+            call.respond(HttpStatusCode.OK, it)
+        } ?: call.respond(HttpStatusCode.NotFound)
     }
     get("/run/{publicId}/summary") {
         val publicId = call.parameters.getOrFail("publicId")
@@ -30,7 +40,6 @@ fun Route.testRuns(testRunService: TestRunService) {
             testRunService.addTestRunSummaryToSpan(testRunSummary)
 
             call.respond(HttpStatusCode.OK, it)
-        }
-            ?: call.respond(HttpStatusCode.NotFound)
+        } ?: call.respond(HttpStatusCode.NotFound)
     }
 }
