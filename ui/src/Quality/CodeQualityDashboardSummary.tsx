0702c5f76c21159bcd91424a181dfe939af87442
Craig Atkinson <atk.craig@gmail.com>
2021-10-16 08:41:18 -0500

    (feature) Adding code quality reports to dashboard

---
+++ ui/src/Quality/CodeQualityDashboardSummary.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import PageTitle from "../PageTitle";
+import { fetchCodeQualityReports } from "../service/TestRunService";
+import { CodeQualityReports } from "../model/TestRunModel";
+import CleanLink from "../Link/CleanLink";
+import { List, ListItem, Typography } from "@material-ui/core";
+
+interface CodeQualityDashboardSummaryProps {
+  publicId: string;
+}
+
+const CodeQualityDashboardSummary = ({
+  publicId,
+}: CodeQualityDashboardSummaryProps) => {
+  const [codeQualityReports, setCodeQualityReports] =
+    React.useState<CodeQualityReports>(null);
+
+  React.useEffect(() => {
+    fetchCodeQualityReports(publicId)
+      .then((response) => {
+        setCodeQualityReports(response.data);
+      })
+      .catch(() => {});
+  }, [setCodeQualityReports]);
+
+  const reportsWithContents =
+    codeQualityReports && codeQualityReports.reports
+      ? codeQualityReports.reports.filter(
+          (report) => report.contents && report.contents.length > 0
+        )
+      : [];
+  const hasReportsWithContents = reportsWithContents.length > 0;
+
+  if (hasReportsWithContents) {
+    return (
+      <div>
+        <CleanLink to={`/tests/${publicId}/quality`}>
+          <PageTitle title="Code quality" testid="code-quality-summary-title" />
+        </CleanLink>
+        <List>
+          {reportsWithContents.map((report, idx) => (
+            <ListItem key={`code-quality-summary-item-${idx}`}>
+              <Typography variant="body2">
+                File:{" "}
+                <CleanLink
+                  to={`/tests/${publicId}/quality/report/${report.idx}`}
+                >
+                  {report.fileName}
+                </CleanLink>
+              </Typography>
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default CodeQualityDashboardSummary;
---
+++ ui/src/Quality/__tests__/CodeQualityDashboardSummary.spec.tsx
@@ -0,0 +1,62 @@
+import MockAdapter from "axios-mock-adapter";
+import { axiosInstance } from "../../service/AxiosService";
+import * as React from "react";
+import { render } from "@testing-library/react";
+import CodeQualityDashboardSummary from "../CodeQualityDashboardSummary";
+import {
+  CodeQualityReport,
+  CodeQualityReports,
+} from "../../model/TestRunModel";
+
+describe("CodeQualityDashboardSummary", () => {
+  let mockAxios;
+
+  beforeEach(() => {
+    // @ts-ignore
+    mockAxios = new MockAdapter(axiosInstance);
+  });
+  afterEach(() => {
+    mockAxios.restore();
+  });
+
+  const publicId = "12345";
+
+  it("should show code quality links when there are reports", async () => {
+    const codeQualityReports = {
+      reports: [
+        {
+          idx: 1,
+          fileName: "report.txt",
+          contents: "Report contents",
+        } as CodeQualityReport,
+      ],
+    } as CodeQualityReports;
+
+    mockAxios
+      .onGet(`http://localhost:8080/run/${publicId}/quality`)
+      .reply(200, codeQualityReports);
+
+    const { findByTestId, findByText } = render(
+      <CodeQualityDashboardSummary publicId={publicId} />
+    );
+
+    await findByTestId("code-quality-summary-title");
+    await findByText("report.txt");
+  });
+
+  it("should not show code quality section when there are no code quality reports", () => {
+    const codeQualityReports = {
+      reports: [],
+    } as CodeQualityReports;
+
+    mockAxios
+      .onGet(`http://localhost:8080/run/${publicId}/quality`)
+      .reply(200, codeQualityReports);
+
+    const { queryByTestId } = render(
+      <CodeQualityDashboardSummary publicId={publicId} />
+    );
+
+    expect(queryByTestId("code-quality-summary-title")).toBeNull();
+  });
+});
