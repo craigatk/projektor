16ff3c1ec47ecebe809773ede205a666c4f200ac
Slava Miligoulo <slava.miligoulo@target.com>
2023-06-21 16:01:14 -0500

    refactor: Updated Kotlin dependencies, gradlew version, and update minio to newer syntax.

--- server/persistence/object-store/src/main/kotlin/projektor/objectstore/ObjectStoreClient.kt
+++ server/persistence/object-store/src/main/kotlin/projektor/objectstore/ObjectStoreClient.kt
@@ -1,41 +1,74 @@
 package projektor.objectstore
 
-import io.minio.MinioClient
+import io.minio.*
 import io.minio.errors.ErrorResponseException
+import io.minio.messages.DeleteObject
 import projektor.objectstore.bucket.BucketCreationException
 import java.io.InputStream
 
-class ObjectStoreClient(private val config: ObjectStoreConfig) {
-    private val minioClient = MinioClient(config.url, config.accessKey, config.secretKey)
+class ObjectStoreClient(config: ObjectStoreConfig) {
+    private val minioClient = MinioClient.builder()
+        .endpoint(config.url)
+        .credentials(config.accessKey, config.secretKey)
+        .build()
 
     fun createBucketIfNotExists(bucketName: String) {
         try {
             if (!bucketExists(bucketName)) {
-                minioClient.makeBucket(bucketName)
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build())
             }
         } catch (e: ErrorResponseException) {
             throw BucketCreationException("Error creating bucket", e)
         }
     }
 
-    fun bucketExists(bucketName: String) = minioClient.bucketExists(bucketName)
+    fun bucketExists(bucketName: String) = minioClient.bucketExists(
+        BucketExistsArgs.builder().bucket(bucketName).build()
+    )
 
     fun putObject(bucketName: String, objectName: String, stream: InputStream) {
-        minioClient.putObject(bucketName, objectName, stream, "application/octet-stream")
+        minioClient.putObject(
+            PutObjectArgs
+                .builder()
+                .bucket(bucketName)
+                .`object`(objectName)
+                .stream(stream, -1, 10485760)
+                .contentType("application/octet-stream")
+                .build()
+        )
     }
 
     fun getObject(bucketName: String, objectName: String): InputStream? =
         try {
-            minioClient.getObject(bucketName, objectName)
+            minioClient.getObject(
+                GetObjectArgs
+                    .builder()
+                    .bucket(bucketName)
+                    .`object`(objectName)
+                    .build()
+            )
         } catch (e: ErrorResponseException) {
             null
         }
 
     fun removeObject(bucketName: String, objectName: String) {
-        minioClient.removeObject(bucketName, objectName)
+        minioClient.removeObject(
+            RemoveObjectArgs
+                .builder()
+                .bucket(bucketName)
+                .`object`(objectName)
+                .build()
+        )
     }
 
     fun removeObjects(bucketName: String, objectNames: List<String>) {
-        minioClient.removeObjects(bucketName, objectNames)
+        val objects = objectNames.map { DeleteObject(it) }
+        minioClient.removeObjects(
+            RemoveObjectsArgs
+            .builder()
+            .bucket(bucketName)
+            .objects(objects)
+            .build()
+        )
     }
 }
