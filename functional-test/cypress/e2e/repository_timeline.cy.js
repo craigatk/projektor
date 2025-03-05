14f09a41c2b2b1eac8d3da51cee041ee20190642
Craig Atkinson <atk.craig@gmail.com>
2025-03-05 06:00:35 -0600

    Try to ensure the second test result is loaded

--- functional-test/cypress/e2e/repository_timeline.cy.js
+++ functional-test/cypress/e2e/repository_timeline.cy.js
@@ -6,47 +6,48 @@ context("repository timeline", () => {
     const repoPart = "timeline-repo";
     const repoName = `${orgPart}/${repoPart}`;
 
-    cy.readFile("cypress/fixtures/grouped-passing-tests-with-git.json")
-      .then((resultsBlob) => {
+    cy.readFile("cypress/fixtures/grouped-passing-tests-with-git.json").then(
+      (resultsBlob) => {
         resultsBlob.metadata.ci = true;
         resultsBlob.metadata.git.repoName = repoName;
 
         cy.loadGroupedFixtureDataAndVisitTestRun(resultsBlob, "");
 
         cy.getByTestId("test-count-list-passed").should("contain", "3");
+      },
+    );
+
+    cy.readFile("cypress/fixtures/grouped-passing-tests-with-git.json")
+      .then((resultsBlob) => {
+        resultsBlob.metadata.ci = true;
+        resultsBlob.metadata.git.repoName = repoName;
+
+        return cy.loadGroupedFixtureData(resultsBlob);
       })
-      .then(() => {
-        cy.readFile("cypress/fixtures/grouped-passing-tests-with-git.json")
-          .then((resultsBlob) => {
-            resultsBlob.metadata.ci = true;
-            resultsBlob.metadata.git.repoName = repoName;
+      .then((publicId) => {
+        cy.visit(`http://localhost:8080/tests/${publicId}`, {
+          retryOnStatusCodeFailure: true,
+        });
 
-            return cy.loadGroupedFixtureData(resultsBlob);
-          })
-          .then((publicId) => {
-            cy.getByTestId("nav-link-repository").click(); // From the test run page
+        cy.getByTestId("nav-link-repository").click(); // From the test run page
 
-            cy.getByTestId("nav-link-repo-timeline").click(); // From the repo page
+        cy.getByTestId("nav-link-repo-timeline").click(); // From the repo page
 
-            cy.getByTestId("repository-timeline-graph");
+        cy.getByTestId("repository-timeline-graph");
 
-            cy.findByRole(`dot-duration-${publicId}`).trigger("mouseover");
+        cy.getByRole(`dot-duration-${publicId}`).trigger("mouseover");
 
-            cy.testIdShouldExist("timeline-graph-tooltip");
+        cy.testIdShouldExist("timeline-graph-tooltip");
 
-            cy.getByTestId("timeline-tooltip-duration").should(
-              "contain",
-              "0.460s",
-            );
-            cy.getByTestId("timeline-tooltip-test-count").should(
-              "contain",
-              "3 tests",
-            );
-            cy.getByTestId("timeline-tooltip-average-duration").should(
-              "contain",
-              "0.153s",
-            );
-          });
+        cy.getByTestId("timeline-tooltip-duration").should("contain", "0.460s");
+        cy.getByTestId("timeline-tooltip-test-count").should(
+          "contain",
+          "3 tests",
+        );
+        cy.getByTestId("timeline-tooltip-average-duration").should(
+          "contain",
+          "0.153s",
+        );
       });
   });
 });
